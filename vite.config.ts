f1fa01adcb495e1f9a8ebf6449a670832882fef1
Victor Miranda <victormiranda13v@gmail.com>
2022-12-20 19:15:10 -0300

    feat: yarn and vite@4.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import VitePluginLinaria from 'vite-plugin-linaria'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import linaria from '@linaria/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginLinaria()]
-})
+  plugins: [react(), linaria()],
+});
